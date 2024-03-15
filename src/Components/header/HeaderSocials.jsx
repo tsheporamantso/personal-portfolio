b71e621107b9637641a84b972edcda823d7d87e9
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-15 07:01:55 +0200

    Add Social meadia component

---
+++ src/Components/header/HeaderSocials.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React from 'react';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaGithub, FaSquareXTwitter } from 'react-icons/fa6';
+
+const HeaderSocials = () => (
+  <div className="header__socials">
+    <>
+      <a href="https://linkedin.com/in/gladwinramantso" target="_blank" rel="noreferrer">
+        <FaLinkedinIn />
+      </a>
+      <a href="https://github.com/tsheporamantso" target="_blank" rel="noreferrer">
+        <FaGithub />
+      </a>
+      <a href="https://twitter.com/ramgt001" target="_blank" rel="noreferrer">
+        <FaSquareXTwitter />
+      </a>
+    </>
+  </div>
+);
+
+export default HeaderSocials;
