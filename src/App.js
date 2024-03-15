6c4f7e22cb5122ee25ec9da676c5342e23afbc33
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-15 02:40:50 +0200

    Delete all files and restart folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import NavBar from './Components/Home/NavBar';
-import './App.css';
 
-const App = () => (
-  <div className="App">
-    <NavBar />
-  </div>
-);
-
-export default App;
