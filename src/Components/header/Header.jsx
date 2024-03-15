ff7240a6abeeeeef82d5f4cfbe48aa7f9bda2d26
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-15 07:03:19 +0200

    Npm install react icons and import header socials component to App.js

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CTA from './CTA';
+import ME from '../../assets/me.jpg';
+import HeaderSocials from './HeaderSocials';
 import './header.css';
 
 const Header = () => (
@@ -9,6 +11,14 @@ const Header = () => (
       <h1>Gladwin Tshepo Ramantso</h1>
       <h5 className="text-light">FullStack Developer</h5>
       <CTA />
+      <HeaderSocials />
+
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+
+      <a href="#contact" className="scroll__down">Scroll Down</a>
+
     </div>
   </header>
 );
