d4fcd7a196dc88156e02d4093f0afc2f7352d10f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-16 06:22:37 +0200

    Add navbar icons

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -1,8 +1,19 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import { FaHome, FaRegUser } from 'react-icons/fa';
+import { IoBookSharp } from 'react-icons/io5';
+import { RiServiceFill } from 'react-icons/ri';
+import { TiMessages } from 'react-icons/ti';
 import './nav.css';
 
 const Nav = () => (
-  <div>Nav</div>
+  <nav>
+    <a href="#home"><FaHome /></a>
+    <a href="#about"><FaRegUser /></a>
+    <a href="#portfolio"><IoBookSharp /></a>
+    <a href="#services"><RiServiceFill /></a>
+    <a href="#contact"><TiMessages /></a>
+  </nav>
 );
 
 export default Nav;
