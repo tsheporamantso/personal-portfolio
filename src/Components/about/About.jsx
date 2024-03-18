64c2a51747cb0b602a18155b3dc9e4dc2e1fb1d3
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-18 13:02:01 +0200

    Add About me title and image

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import ME from '../../assets/me.jpg';
 import './about.css';
 
 const About = () => (
-  <section id="about">About</section>
+  <section id="about">
+    <h5>Get to know</h5>
+    <h2>About Me</h2>
+
+    <div className="container about__container">
+      <div className="about__me">
+        <div className="about__me-image">
+          <img src={ME} alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
 );
 
 export default About;
