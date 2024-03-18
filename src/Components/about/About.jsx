21ae905a96e99d4fe1356fbaa5d279c8a3326426
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-18 10:40:13 +0200

    Add about me container

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './about.css';
 
 const About = () => (
-  <section id="about">About</section>
+  <section id="about">
+    <h5>Get to know</h5>
+    <h2>About Me</h2>
+
+    <div className="container about__container">
+      <div className="about__me">
+        
+      </div>
+    </div>
+  </section>
 );
 
 export default About;
