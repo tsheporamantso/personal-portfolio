3c90c49fe3cb1634b4aebb1baa407bdb2bdf2124
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-19 16:52:26 +0200

    Map through data and render jsx

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,14 +1,62 @@
 import React from 'react';
 import './portfolio.css';
-import IMG1 from '../../assets/Space.png';
+import IMG1 from '../../assets/space.png';
+import IMG2 from '../../assets/staysphare.png';
+import IMG3 from '../../assets/recipe.png';
+import IMG4 from '../../assets/leaderboard.png';
+import IMG5 from '../../assets/bookstore.png';
+import IMG6 from '../../assets/mathmagician.png';
+import IMG7 from '../../assets/chatalpha.png';
 
 const data = [
   {
     id: 1,
-    image: 'IMG1',
+    image: IMG1,
     title: 'Space Travellers Hub',
     github: 'https://github.com/tsheporamantso/Space-Travelers',
     demo: 'https://space-travellers-6soy.onrender.com/',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'StaySphare',
+    github: 'https://github.com/tsheporamantso/final-capstone-react-front-end',
+    demo: 'https://github.com/tsheporamantso/final-capstone-react-front-end',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Recipe App',
+    github: 'https://github.com/tsheporamantso/Recipe-App',
+    demo: 'https://recipeapp-sxaw.onrender.com',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Leader Board',
+    github: 'https://github.com/tsheporamantso/budget-app',
+    demo: 'https://tsheporamantso.github.io/Leaderboard/dist',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Book Store',
+    github: 'https://github.com/tsheporamantso/bookstore',
+    demo: 'https://bookstore-hzhe.onrender.com/',
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Math Magician',
+    github: 'https://github.com/tsheporamantso/bookstore',
+    demo: 'https://math-magicians-app-eljm.onrender.com',
+  },
+  {
+    id: 7,
+    image: IMG7,
+    title: 'Chat Alpha',
+    github: 'https://github.com/tsheporamantso/Chat-Alpha',
+    demo: 'https://github.com/tsheporamantso/Chat-Alpha',
   },
 ];
 
@@ -18,17 +66,22 @@ const Portfolio = () => (
     <h2>Portfolio</h2>
 
     <div className="container portfolio__container">
-      <article className="portfolio__item">
-        <div className="portfolio__item-image">
-          <img src={IMG1} alt="Space Travellors" />
-        </div>
-        <h3>Space Travellers&apos; Hub</h3>
-        <div className="portfolio__item-cta">
-          <a href="https://github.com/tsheporamantso/Space-Travelers" className="btn">GitHub</a>
-          <a href="https://space-travellers-6soy.onrender.com/" className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
-        </div>
-      </article>
-
+      {
+        data.map(({
+          id, image, title, github, demo,
+        }) => (
+          <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className="btn">GitHub</a>
+              <a href={demo} className="btn btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+            </div>
+          </article>
+        ))
+}
     </div>
   </section>
 );
