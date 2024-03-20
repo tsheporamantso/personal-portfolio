010ea4de4b3fa8cc5c4807fafc3426305628eb5b
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-20 17:12:59 +0200

    Add Angellist on socials and removed twitter

--- src/Components/header/HeaderSocials.jsx
+++ src/Components/header/HeaderSocials.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import { FaLinkedinIn } from 'react-icons/fa';
-import { FaGithub, FaSquareXTwitter, FaDribbble } from 'react-icons/fa6';
+import { FaLinkedinIn, FaAngellist } from 'react-icons/fa';
+import { FaGithub, FaDribbble } from 'react-icons/fa6';
 
 const HeaderSocials = () => (
   <div className="header__socials">
@@ -12,8 +12,8 @@ const HeaderSocials = () => (
       <a href="https://github.com/tsheporamantso" target="_blank" rel="noreferrer">
         <FaGithub />
       </a>
-      <a href="https://twitter.com/ramgt001" target="_blank" rel="noreferrer">
-        <FaSquareXTwitter />
+      <a href="https://wellfound.com/u/gladwin-tshepo-ramantso" target="_blank" rel="noreferrer">
+        <FaAngellist />
       </a>
       <a href="https://dribbble.com/TshepoRamantso" target="_blank" rel="noreferrer">
         <FaDribbble />
