49e23646569502bb40b1ede7f1b76605d38481b0
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-27 10:01:46 +0200

    Correct placement of nav link

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -13,8 +13,8 @@ const Nav = () => {
     <nav>
       <a href="#home" onClick={() => { setActiveNav('#home'); }} className={activeNav === '#home' ? 'active' : ''}><FaHome /></a>
       <a href="#about" onClick={() => { setActiveNav('#about'); }} className={activeNav === '#about' ? 'active' : ''}><FaRegUser /></a>
-      <a href="#portfolio" onClick={() => { setActiveNav('#portfolio'); }} className={activeNav === '#portfolio' ? 'active' : ''}><IoBookSharp /></a>
       <a href="#services" onClick={() => { setActiveNav('#services'); }} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
+      <a href="#portfolio" onClick={() => { setActiveNav('#portfolio'); }} className={activeNav === '#portfolio' ? 'active' : ''}><IoBookSharp /></a>
       <a href="#contact" onClick={() => { setActiveNav('#contact'); }} className={activeNav === '#contact' ? 'active' : ''}><TiMessages /></a>
     </nav>
   );
