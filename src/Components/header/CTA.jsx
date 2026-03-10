faea8c5909dd6e15ea244bea7b162e8676fdb6fd
tsheporamantso <tshepo.ramantso@outlook.com>
2026-03-10 10:20:05 +0200

    Add updated resume link on CTA button

--- src/Components/header/CTA.jsx
+++ src/Components/header/CTA.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import Resume from '../../assets/Resume.pdf';
+import React from "react";
+import Resume from "../../assets/Tshepo_Ramantso_FullStack_Developer.pdf";
 
 const CTA = () => (
   <div className="cta">
     <>
-      <a href={Resume} download className="btn">Download Resume</a>
-      <a href="#contact" className="btn btn-primary">Lets&apos;s Talk</a>
+      <a href={Resume} download className="btn">
+        Download Resume
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Lets&apos;s Talk
+      </a>
     </>
   </div>
 );
