ff74816059c069bc7695c20251ed438dfcd8185d
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-01 15:00:55 +0200

    Add reusable base url object to keep code DRY

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,7 @@ export const BASE_URL = process.env.REACT_APP_API_URL;
 const API = {
   testimonials: `${BASE_URL}/api/v1/testimonials`,
   projects: `${BASE_URL}/api/v1/projects`,
+  services: `${BASE_URL}/api/v1/services`,
 };
 
 export default API;
