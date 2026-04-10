f110bf72bf7493af38b830c4aebe0370fcddb1ef
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-10 23:07:08 +0200

    feat: wrap app with browser router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <App />,
+  <BrowserRouter>
+    <App />,
+  </BrowserRouter>,
 );
