50d594c2ce87bed1e8e1d2eebfdc611579b638e4
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-11 09:07:48 +0200

    feat(nav): add navigation link to login route

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -2,8 +2,10 @@
 import React, { useState } from 'react';
 import { FaHome, FaRegUser } from 'react-icons/fa';
 import { IoBookSharp } from 'react-icons/io5';
-import { RiServiceFill } from 'react-icons/ri';
+import { RiServiceFill, RiLoginBoxLine } from 'react-icons/ri';
 import { TiMessages } from 'react-icons/ti';
+import { Link } from 'react-router-dom';
+
 import './nav.css';
 
 const Nav = () => {
@@ -11,11 +13,61 @@ const Nav = () => {
 
   return (
     <nav>
-      <a href="#home" onClick={() => { setActiveNav('#home'); }} className={activeNav === '#home' ? 'active' : ''}><FaHome /></a>
-      <a href="#about" onClick={() => { setActiveNav('#about'); }} className={activeNav === '#about' ? 'active' : ''}><FaRegUser /></a>
-      <a href="#services" onClick={() => { setActiveNav('#services'); }} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
-      <a href="#portfolio" onClick={() => { setActiveNav('#portfolio'); }} className={activeNav === '#portfolio' ? 'active' : ''}><IoBookSharp /></a>
-      <a href="#contact" onClick={() => { setActiveNav('#contact'); }} className={activeNav === '#contact' ? 'active' : ''}><TiMessages /></a>
+      <a
+        href="#home"
+        title="Home"
+        onClick={() => {
+          setActiveNav('#home');
+        }}
+        className={activeNav === '#home' ? 'active' : ''}
+      >
+        <FaHome />
+      </a>
+      <a
+        href="#about"
+        title="About"
+        onClick={() => {
+          setActiveNav('#about');
+        }}
+        className={activeNav === '#about' ? 'active' : ''}
+      >
+        <FaRegUser />
+      </a>
+      <a
+        href="#services"
+        title="Services"
+        onClick={() => {
+          setActiveNav('#services');
+        }}
+        className={activeNav === '#services' ? 'active' : ''}
+      >
+        <RiServiceFill />
+      </a>
+      <a
+        href="#portfolio"
+        title="Portfolio"
+        onClick={() => {
+          setActiveNav('#portfolio');
+        }}
+        className={activeNav === '#portfolio' ? 'active' : ''}
+      >
+        <IoBookSharp />
+      </a>
+      <a
+        href="#contact"
+        title="Contact Me"
+        onClick={() => {
+          setActiveNav('#contact');
+        }}
+        className={activeNav === '#contact' ? 'active' : ''}
+      >
+        <TiMessages />
+      </a>
+      <li>
+        <Link to="/login" title="Admin Login">
+          <RiLoginBoxLine />
+        </Link>
+      </li>
     </nav>
   );
 };
