ad63707f2972a19d5ce76199fac78030d87ccb57
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-21 14:23:38 +0200

    chore(api): add tips and tips checkout API endpoints

---
+++ src/Components/TipJar/TipJar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TipJar = () => {
+  return <div>TipJar</div>;
+};
+
+export default TipJar;
--- src/utils/api.js
+++ src/utils/api.js
@@ -6,6 +6,8 @@ const API = {
   services: `${BASE_URL}/api/v1/services`,
   contacts: `${BASE_URL}/api/v1/contacts`,
   login: `${BASE_URL}/api/v1/auth/login`,
+  createTipCheckout: `${BASE_URL}/api/v1/tips/create-checkout`,
+  tips: `${BASE_URL}/api/v1/tips`,
   deleteContact: (id) => `${BASE_URL}/api/v1/contacts/${id}`,
 };
 
