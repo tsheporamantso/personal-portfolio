fdf2bbcbdbb158214cd5720b2ba36245ae38c23a
tsheporamantso <tshepo.ramantso@outlook.com>
2026-04-23 14:29:14 +0200

    chore(api): add logout API endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,6 +6,7 @@ const API = {
   services: `${BASE_URL}/api/v1/services`,
   contacts: `${BASE_URL}/api/v1/contacts`,
   login: `${BASE_URL}/api/v1/auth/login`,
+  logout: `${BASE_URL}/api/v1/auth/logout`,
   createTipCheckout: `${BASE_URL}/api/v1/tips/create-checkout`,
   tips: `${BASE_URL}/api/v1/tips`,
   deleteContact: (id) => `${BASE_URL}/api/v1/contacts/${id}`,
