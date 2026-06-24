e64655d6c36cd0a02448e9449bb03f3838a519d0
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-24 14:03:22 +0200

    test(About): correct typing error

--- src/Components/about/About.test.jsx
+++ src/Components/about/About.test.jsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 
 import About from './About';
 
-describe('renders the component correctly', () => {
-  test('it renders the main heading and subheading correctly', () => {
+describe('About component', () => {
+  test('should renders the main heading and subheading correctly', () => {
     render(<About />);
 
     const heading = screen.getByText(/get to know/i);
@@ -12,7 +12,7 @@ describe('renders the component correctly', () => {
     const subHeading = screen.getByText(/about me/i);
     expect(subHeading).toBeTruthy();
   });
-  test('it render the headshot image with correct alt text', () => {
+  test('should render the headshot image with correct alt text', () => {
     render(<About />);
     const image = screen.getByAltText(/about me/i);
     expect(image).toBeInTheDocument();
