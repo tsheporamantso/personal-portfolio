5e95c907a22f661206993c0edf6e0ab92f5d1ce6
tsheporamantso <tshepo.ramantso@outlook.com>
2026-06-30 07:20:05 +0200

    refactor: update query methods for accessibility

--- src/Components/about/About.test.jsx
+++ src/Components/about/About.test.jsx
@@ -6,10 +6,16 @@ describe('About component', () => {
   test('should renders the main heading and subheading correctly', () => {
     render(<About />);
 
-    const heading = screen.getByText(/get to know/i);
+    const heading = screen.getByRole('heading', {
+      name: /get to know/i,
+      level: 5,
+    });
     expect(heading).not.toBeNull();
 
-    const subHeading = screen.getByText(/about me/i);
+    const subHeading = screen.getByRole('heading', {
+      name: /about me/i,
+      level: 2,
+    });
     expect(subHeading).toBeTruthy();
   });
   test('should render the headshot image with correct alt text', () => {
