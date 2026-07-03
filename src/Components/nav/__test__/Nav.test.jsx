b9417a136d107d64ac97d83c2408b1e3f611340c
tsheporamantso <tshepo.ramantso@outlook.com>
2026-07-03 15:15:52 +0200

    test(nav bar): add nav links test cases and snapshots

---
+++ src/Components/nav/__test__/Nav.test.jsx
@@ -0,0 +1,39 @@
+import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Nav from '../Nav';
+
+const renderNav = () => {
+  render(
+    <MemoryRouter future={{ v7_relativeSplatPath: true }}>
+      <Nav />
+    </MemoryRouter>,
+  );
+};
+
+describe('Navbar component', () => {
+  test('should render the correct number of links', () => {
+    renderNav();
+
+    const navLinks = screen.getAllByRole('link');
+    expect(navLinks).toHaveLength(7);
+  });
+
+  test.each([
+    ['#home', 'Home'],
+    ['#about', 'About'],
+    ['#services', 'Services'],
+    ['#portfolio', 'Portfolio'],
+    ['#contact', 'Contact Me'],
+    ['/tip', 'Buy me a coffee'],
+    ['/login', 'Admin Login'],
+  ])('should render %s link with correct title', (href, title) => {
+    renderNav();
+
+    const link = screen.getByTitle(title);
+
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', href);
+
+    expect(link).toMatchSnapshot();
+  });
+});
